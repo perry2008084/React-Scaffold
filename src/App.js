19f09279bcf2f05fd77124b8127948dc48cf601f
ACPerry <pansj@acloudchina.com>
2019-04-11 16:38:19 +0800

    modify warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import AddToDo from './containers/AddToDo';
